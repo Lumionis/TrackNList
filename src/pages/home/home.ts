a4c3dbd2f56f38610f3aece8757eedb9e38bf82f
Florian Buet <florian.buet@ynov.com>
2019-03-20 10:24:47 +0100

    Affichage de musique sur la page principale

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,12 +9,35 @@ export class HomePage {
 
   test : String;
   user : String;
+  source : String;
+
+  music =[
+    {
+      titre: 'khapta',
+      album: 'En Esprit',
+      artiste: 'Heuss l\'enfoiré',
+      image: 'https://t2.genius.com/unsafe/220x220/https%3A%2F%2Fimages.genius.com%2F3d67323ac039e1363d6cd29de0d997c8.649x649x1.gif'
+    },
+    {
+      titre: 'PGP',
+      album: 'PgP',
+      artiste: 'Booba',
+      image: 'https://www.raprnb.com/wp-content/uploads/2019/01/VISU-LYRICS-booba-pgp.jpg'
+    },
+    {
+      titre: 'Trop Beau',
+      album: 'Jeannine',
+      artiste: 'Lomepal',
+      image: 'http://www.handsandarms.com/15238-19334-large_default/lomepal-lpx2-jeannine.jpg'
+    }
+  ]
 
 
   constructor(public navCtrl: NavController) {
 
     this.test = "Nom de la playlist";
-    this.user = "Nom du User ";
+    this.user = "Florian ";
+    this.source = "../../assets/imgs/smog.jpg";
   }
 
 }
